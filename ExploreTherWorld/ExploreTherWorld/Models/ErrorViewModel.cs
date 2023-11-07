a804e56b87b35c13eea0776dba6c4c425752828a
publishingword55@gmail.com <publishingword55@gmail.com>
2023-11-08 01:01:44 +0530

    App Creation

---
+++ ExploreTherWorld/ExploreTherWorld/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ExploreTherWorld.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
